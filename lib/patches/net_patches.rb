8a27391484550d7cf6f71526c81ec5c9e110dd59
Jacob Kjeldahl <jkj@nettopower.dk>
2016-07-04 11:55:42 +0200

    Adds profiling to Net::IMAP and Mail::SMTP

--- lib/patches/net_patches.rb
+++ lib/patches/net_patches.rb
@@ -12,3 +12,31 @@ if (defined?(Net) && defined?(Net::HTTP))
   end
 
 end
+
+if (defined?(Net) && defined?(Net::IMAP))
+
+  Net::IMAP.class_eval do
+    def send_command_with_mini_profiler(cmd, *args, &block)
+      Rack::MiniProfiler.step("Net::IMAP #{cmd}") do
+        send_command_without_mini_profiler(cmd, *args, &block)
+      end
+    end
+    alias send_command_without_mini_profiler send_command
+    alias send_command send_command_with_mini_profiler
+  end
+
+end
+
+if (defined?(Mail) && defined?(Mail::SMTP))
+
+  Mail::SMTP.class_eval do
+    def deliver_with_mini_profiler(mail)
+      Rack::MiniProfiler.step("Mail::SMTP #{settings[:address]}:#{settings[:port]}") do
+        deliver_without_mini_profiler(mail)
+      end
+    end
+    alias deliver_without_mini_profiler deliver!
+    alias deliver! deliver_with_mini_profiler
+  end
+
+end
